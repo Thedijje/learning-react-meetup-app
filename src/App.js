969856e4c09c0127f8faea2a53f4925a1706eed6
Dheeraj jha <dheeraj@thedijje.com>
2022-02-10 17:35:49 +0530

    cleanup done for new project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
       <h1>My TODO!</h1>
-      <Todo title="Learn React" />
-      <Todo title="Master React" />
-      <Todo title="Complete React course" />
     </div>
   );
 }
